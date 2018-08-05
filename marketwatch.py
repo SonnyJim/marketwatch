f4288f6639d69efc88116676e6d1db0cf1d38552
SonnyJim <sonny_jim_pin@yahoo.com>
2018-08-05 22:34:30 +1000

    Add in evemail and various other jibber jabs

--- marketwatch.py
+++ marketwatch.py
@@ -9,6 +9,9 @@ import pickle
 import requests
 import json
 import threading
+import smtplib
+from email.message import EmailMessage
+
 from time import sleep
 from config import *
 
@@ -26,13 +29,43 @@ def get_name_from_id (type_id):
 def order_list_remove (order_id):
     print ("Removing " + str(order_id) + " from orders_list")
 
-#TODO
-def open_ui_for_itemid (item_id):
-    print ("Opening UI for item_id " + str(item_id))
+def open_ui_for_type_id (type_id):
+    print ("ui: Opening UI for item_id " + str(type_id))
+    op = app.op['post_ui_openwindow_marketdetails'](type_id=type_id)
+    ui = client.request(op)
+
+    #if (ui.response != 204):
+    #    print ("ui: something went wrong error "+ str(ui.code))
+    #    return 1
 
 #TODO
-def send_notification (subject, body):
-    print ("Sending notification")
+def send_evemail (subject, body):
+    print ("Sending evemail")
+    mail = {'approved_cost':10000, 'body':body, 'subject':subject, 'recipients':[{'recipient_id':corporation_id, 'recipient_type':'corporation'}]}
+    op = app.op ['post_characters_character_id_mail'](
+            character_id=character_id,
+            mail=mail)
+    r = client.request(op)
+    print (r.data)
+
+def send_email (subject, body):
+    msg = EmailMessage()
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = subject
+    msg.set_content (body)
+    server = smtplib.SMTP('localhost')
+    server.send_message (msg)
+    server.quit()
+
+def send_notification (subject, body, type_id):
+    print ("notify: Sending notification " + subject)
+    if notify_by_email:
+        send_email (subject, body)
+    if notify_by_evemail:
+        send_evemail (subject, body)
+    if notify_by_ui:
+        open_ui_for_type_id (type_id)
 
 def get_corp_orders():
     name = get_name_from_id (corporation_id)
@@ -51,7 +84,7 @@ def get_corp_orders():
     #Find the orders and add them to the list if they aren't there already
     for order in orders.data:
         if order['location_id'] == location_id and order['order_id'] not in order_list:
-                print ("orders:  Found new order_id " + str(order['order_id']))
+                print ("orders: Found order_id " + str(order['order_id']))
                 
                 if 'is_buy_order' in order:
                     is_buy_order = True
@@ -64,19 +97,23 @@ def get_corp_orders():
                 threads.append(t)
                 t.start()
     
-
+#Monitors an order_id for changes
 def monitor_order (order_mine, is_buy_order):
 
     name = get_name_from_id (order_mine['type_id'])
     if is_buy_order:
-        buy_msg = "buy orders"
+        buy_msg = "buy order"
     else:
-        buy_msg = "sell orders"
+        buy_msg = "sell order"
 
-    print ("monitor: Starting monitor for " + buy_msg + " of " + name + "'s (order_id " + str(order_mine['order_id']) + ")")
+    print ("monitor: Starting monitor for " + buy_msg + " of " + name + "s (order_id " + str(order_mine['order_id']) + ")")
     
     #Start off assuming that we have the best price already
     best_price = order_mine['price']
+
+    volume_remain_old = order_mine['volume_remain']
+
+    sent_notification = False
     
     while (order_mine['volume_remain'] > 0):
         #Fetch all the orders for the region
@@ -107,16 +144,37 @@ def monitor_order (order_mine, is_buy_order):
 
         #Check to see if we actually found our own order
         if len(order_mine) == 0 or order_mine_exists == False:
-            print ("monitor: Couldn't find my order_id: "+ str(order_mine['order_id']) + ", maybe it's sold out")
+            print ("monitor: Couldn't find my order_id: "+ str(order_mine['order_id']) + " " + name + ", maybe it's sold out")
+            subject = "EVE Marketwatch: sold out of " + name
+            body = "I can't find order id " + str(order_mine['order_id']) + ", looks like we sold all " + str(order_mine['volume_total']) + " of the " + name + " we had listed"
+            send_notification (subject, body, order_mine['type_id'])
             return 1
 
         #Notify us about the current state of our order
-        print ("monitor: " + name + " best price: " + str(best_price) + ", my price: " +str(order_mine['price']))
-        if is_buy_order and best_price > order_mine['price']:
-            print ("monitor: Not winning " + name + " buy order, use price: " + str(best_price + 0.01))
+        #print ("monitor: " + name + " best price: " + str(best_price) + ", my price: " +str(order_mine['price']))
 
-        if is_buy_order == False and best_price < order_mine['price']:
-            print ("monitor: Not winning " + name + " sell order, use price: " + str(best_price - 0.01))
+        if order_mine['volume_remain'] < volume_remain_old:
+            volume_remain_old = order_mine['volume_remain']
+            print ("monitor: " + name + " " + str(order_mine['volume_remain']) + "/" + str(order_mine['volume_total']))
+
+        #Reset the sent_notification if we are currently the best price
+        if best_price == order_mine['price']:
+            sent_notification = False
+        
+        if is_buy_order and best_price > order_mine['price'] and sent_notification != True:
+            subject = "EVE Marketwatch: " + name
+            body = "monitor: Not winning " + name + " buy order, use price: " + str(best_price + 0.01)
+            print (body)
+            send_notification (subject, body, order_mine['type_id'])
+            sent_notification = True
+
+
+        if is_buy_order == False and best_price < order_mine['price'] and sent_notification != True:
+            subject = "EVE Marketwatch: " + name
+            body = "monitor: Not winning " + name + " sell order, use price: " + str(best_price - 0.01)
+            print (body)
+            send_notification (subject, body, order_mine['type_id'])
+            sent_notification = True
 
         sleep(30)
         
